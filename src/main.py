b3c4a5c66476f5dd13c192e857844356eeac900d
Ruriel <rurielsantos@scio.solutions.com.br>
2021-05-23 20:42:36 -0300

    -Adding pyspark to Pipfile. -Adding date format to time duration field. -Adding methods to read the generated json file into a dataframe. -Adding method to convert the column names and value types of the dataframe into the ones specified in the metadata. -Adding method to drop the columns not present in the metadata. -Adding UTC timezone to the session loader.

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ from spark_operations.etl import ETL
 if __name__ == "__main__":
     etl = ETL()
     fileLocation = etl.pre_processing('./datalake/generated.json.gz')
-    print(fileLocation)
+    etl.load_data(fileLocation)
+    etl.convert_dataframe()
---
+++ src/spark_operations/spark_session_loader.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+class _SparkSessionLoader:
+    def __init__(self):
+        self._spark_session = SparkSession \
+            .builder \
+            .appName("Spark_ETL_Server") \
+            .getOrCreate()
+        self._spark_session.conf.set("spark.sql.shuffle.partitions", 5)
+        self._spark_session.conf.set("spark.sql.session.timeZone", "UTC")
+    
+    def get_spark_session(self):
+        return self._spark_session
