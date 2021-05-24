fbc913685f55e21a0076f425575d67b0daff02ad
Ruriel <rurielsantos@scio.solutions.com.br>
2021-05-24 18:11:18 -0300

    -Moving dot env loading to main module. -Adding API module to send data to Druid. -Adding druidServer env variable. -Changing JSON output format by setting one object per line instead of a single JSON array. -Adding date format on metadata.

---
+++ src/rest/api.py
@@ -0,0 +1,66 @@
+import requests
+from os.path import dirname, basename
+from os import getenv
+
+
+def _prepareRequest(metadata, jsonFolder):
+    dimensions = []
+    timestampColumn = None
+
+    for column in metadata['fact']['dimensions']:
+        if "isTimeDimension" in column and column['isTimeDimension']:
+            timestampColumn = {
+                "column": column["name"],
+                "format": column["format"]
+            }
+        else:
+            if("type" not in column or type(column["type"]) is str):
+                fieldType = "string" if "type" not in column else column["type"]
+            else:
+                fieldType = column["type"]["destination"]
+            dimensions.append({
+                "type": fieldType,
+                "name": column["name"]
+            })
+
+    requestBody = {
+        "type": "index_parallel",
+        "spec": {
+            "dataSchema": {
+                "dataSource": metadata['fact']['name'],
+                "timestampSpec": timestampColumn,
+                "dimensionsSpec": {
+                    "dimensions": dimensions,
+                    "dimensionExclusions" : [timestampColumn["column"]]
+                },
+                "granularitySpec": {
+                    "segmentGranularity": "day",
+                    "queryGranularity": "none",
+                    "rollup": True
+                }
+            },
+            "ioConfig": {
+                "type": "index_parallel",
+                "inputSource": {
+                    "type": "local",
+                    "baseDir": dirname(jsonFolder),
+                    "filter": basename(jsonFolder)
+                },
+                "inputFormat": {
+                    "type": "json"
+                }
+            },
+            "tuningConfig": {
+                "type": "index_parallel"
+            }
+        }
+    }
+    return requestBody
+
+
+def sendToDruid(metadata, jsonFolder):
+    body = _prepareRequest(metadata, jsonFolder)
+    druidServer = getenv("druidServer")
+    response = requests.post(druidServer, json=body)
+    print('Response: {}'.format(response.text))
+    return response
--- src/sparkOperations/metadataLoader.py
+++ src/sparkOperations/metadataLoader.py
@@ -1,11 +1,8 @@
-from dotenv import load_dotenv as loadDotEnv
 from json import load as jsonLoad
 from os import getenv
 
-
 class _MetadataLoader:
     def __init__(self):
-        loadDotEnv()
         metadataFile = open(getenv('metadataFile'))
         self._metadata = jsonLoad(metadataFile)
         metadataFile.close()
